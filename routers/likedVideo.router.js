60b26c2a7ac5f6b1ccf7b7c054cbd0e2f77ea6d9
Ajith <ajit.v.billava@gmail.com>
2021-09-18 00:11:10 +0530

    dd

--- routers/likedVideo.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getLikedVideos, addToLikedVideos, removeFromLikedVideos } = require("../controllers/likedVideos.controller");
-const checkAuth = require("../middelware/checkAuth");
-const likedVideosRouter = express.Router();
-
-likedVideosRouter.route("/:userId/likedVideos").all(checkAuth).get(getLikedVideos).post(addToLikedVideos)
-likedVideosRouter.route("/:userId/likedVideos/:videoId/remove").all(checkAuth).post(removeFromLikedVideos)
-
-module.exports = likedVideosRouter
--- routers/video.router.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { getAllVideos, postVideo } = require("../controllers/video.controller");
-const videoRouter = express.Router();
-
-videoRouter.route("/").get(getAllVideos).post(postVideo);
-
-module.exports = videoRouter
