5e30dc24916aac382c45683933a7fa53d5987803
Ajith <ajit.v.billava@gmail.com>
2021-09-18 11:03:26 +0530

    Initial commit

--- routers/savedVideos.router.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getSavedVideos, addToSavedVideos, removeFromSavedVideos } = require("../controllers/savedVideos.controller");
-const checkAuth = require("../middelware/checkAuth");
-const savedVideosRouter = express.Router();
-
-savedVideosRouter.route("/:userId/savedVideos/").all(checkAuth).get(getSavedVideos).post(addToSavedVideos)
-savedVideosRouter.route("/:userId/savedVideos/:videoId/remove").all(checkAuth).post(removeFromSavedVideos)
-
-module.exports  = savedVideosRouter
